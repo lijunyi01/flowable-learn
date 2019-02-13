2067518c0909571be388e57196ee80a2b3305c83
liteyi <liteyi@handlecar.com>
2019-02-13 11:21:36 +0800

    modify

---
+++ modules/03-spring-app-2/src/main/java/com/allcom/entity/Person.java
@@ -0,0 +1,73 @@
+package com.allcom.entity;
+
+
+import java.sql.Date;
+
+public class Person {
+
+  private Integer personid;
+  private String username;
+  private String firstname;
+  private String lastname;
+  private Date birthdate;
+
+  public Person(String username, String firstname, String lastname, Date birthdate) {
+    this.username = username;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.birthdate = birthdate;
+  }
+
+  public Person(Integer personid, String username, String firstname, String lastname, Date birthdate) {
+    this.personid = personid;
+    this.username = username;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.birthdate = birthdate;
+  }
+
+  public Integer getPersonid() {
+    return personid;
+  }
+
+  public void setPersonid(Integer personid) {
+    this.personid = personid;
+  }
+
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+
+  public Date getBirthdate() {
+    return birthdate;
+  }
+
+  public void setBirthdate(java.sql.Date birthdate) {
+    this.birthdate = birthdate;
+  }
+
+}
